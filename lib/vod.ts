42432201aba4553d44923c97dd5088ae9fb82207
schamrockt <k4yr225@gmail.com>
2025-02-11 19:30:22 +0300

    Update Vod class methods to include type aliases for VodDo, VodIf, and VodElse, enhancing type safety and clarity

--- lib/vod.ts
+++ lib/vod.ts
@@ -1,26 +1,26 @@
 import VodObject from "./vodObject";
 import VodBody from "./vodBody";
-import VodDo from "./objects/vodDo";
-import VodIf, { VodIfOther } from "./objects/vodIf";
-import VodElse from "./objects/vodElse";
+import VodDo, { VodDoType } from "./objects/vodDo";
+import VodIf, { VodIfType,VodIfOther } from "./objects/vodIf";
+import VodElse, { VodElseType } from "./objects/vodElse";
 
 export class Vod {
 
-    public static do<TValue>(path : string, value : TValue, modus? : string) : VodObject<TValue, VodDo<TValue>> {
+    public static do<TValue>(path : string, value : TValue, modus? : string) : VodObject<TValue, VodDoType, VodDo<TValue>> {
         return {
             type: "do",
             content: [path, value, modus] as VodDo<TValue>
         }
     }
 
-    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody, other? : VodIfOther<TValue>) : VodObject<TValue, VodIf<TValue>> {
+    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody, other? : VodIfOther<TValue>) : VodObject<TValue, VodIfType, VodIf<TValue>> {
         return {
             type: "if",
             content: [path, modus, value, body, other] as VodIf<TValue>
         }
     }
 
-    public static else<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodObject<TValue, VodElse<TValue>> {
+    public static else<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodObject<TValue, VodElseType, VodElse<TValue>> {
         return {
             type: "else",
             content: [path, modus, value, body] as VodElse<TValue>
