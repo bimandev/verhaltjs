09e6e6b15e6453c52d961866e538a48756a48571
schamrockt <k4yr225@gmail.com>
2025-02-11 19:10:23 +0300

    Update VodIf type definition to include optional VodIfContinue for enhanced conditional logic

--- lib/objects/vodIf.ts
+++ lib/objects/vodIf.ts
@@ -1,5 +1,8 @@
 import VodBody from "../vodBody";
+import VodElseIf from "./vodElseIf";
 
-export type VodIf<TValue> = [string, string, TValue, VodBody];
+export type VodIf<TValue> = [string, string, TValue, VodBody, VodIfContinue<TValue>?];
 
 export default VodIf;
+
+export type VodIfContinue<TValue> = VodElseIf<TValue>;
