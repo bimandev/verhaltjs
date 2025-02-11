74beb9fb6475e24073e78b5b2a46f6abfba7cb87
schamrockt <k4yr225@gmail.com>
2025-02-11 19:23:28 +0300

    Update Vod class methods to support generic content types for improved type safety and flexibility

--- lib/vod.ts
+++ lib/vod.ts
@@ -6,21 +6,21 @@ import VodElse from "./objects/vodElse";
 
 export class Vod {
 
-    public static do<TValue>(path : string, value : TValue, modus? : string) : VodObject<TValue> {
+    public static do<TValue>(path : string, value : TValue, modus? : string) : VodObject<TValue, VodDo<TValue>> {
         return {
             type: "do",
             content: [path, value, modus] as VodDo<TValue>
         }
     }
 
-    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody, other? : VodIfOther<TValue>) : VodObject<TValue> {
+    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody, other? : VodIfOther<TValue>) : VodObject<TValue, VodIf<TValue>> {
         return {
             type: "if",
             content: [path, modus, value, body, other] as VodIf<TValue>
         }
     }
 
-    public static else<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodObject<TValue> {
+    public static else<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodObject<TValue, VodElse<TValue>> {
         return {
             type: "else",
             content: [path, modus, value, body] as VodElse<TValue>
