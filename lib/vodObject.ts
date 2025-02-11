2e84a70e7901b9198e263479766ad57e86dc5a01
schamrockt <k4yr225@gmail.com>
2025-02-11 19:29:51 +0300

    Enhance VodObject type to include VodElse and VodElseIf, improving type safety and flexibility

--- lib/vodObject.ts
+++ lib/vodObject.ts
@@ -1,13 +1,15 @@
-import VodDo from "./objects/vodDo";
-import VodIf from "./objects/vodIf";
+import VodDo, { VodDoType } from "./objects/vodDo";
+import VodIf, { VodIfType } from "./objects/vodIf";
+import VodElseIf, { VodElseIfType } from "./objects/vodElseIf";
+import VodElse, { VodElseType } from "./objects/vodElse";
 
-export type VodObject<TValue, TContent extends VodObjectContent<TValue>> = {
-    type : VodObjectType;
+export type VodObject<TValue, TType extends VodObjectType, TContent extends VodObjectContent<TValue>> = {
+    type : TType;
     content : TContent;
 }
 
 export default VodObject;
 
-export type VodObjectType = "do" | "if" | "elseif" | "else";
+export type VodObjectType = VodDoType | VodIfType | VodElseIfType | VodElseType;
 
-export type VodObjectContent<TValue> = VodDo<TValue> | VodIf<TValue>;
+export type VodObjectContent<TValue> = VodDo<TValue> | VodIf<TValue> | VodElseIf<TValue> | VodElse<TValue>;
