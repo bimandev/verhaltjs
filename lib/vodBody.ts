25bdf9dd40bd5d7c20fa4bee7dde6bb30aa25870
schamrockt <k4yr225@gmail.com>
2025-02-11 19:43:18 +0300

    Refactor VodBody type definition to include VodBodyContent for improved clarity and type safety

--- lib/vodBody.ts
+++ lib/vodBody.ts
@@ -1,5 +1,9 @@
-import VodObject, { VodObjectContent } from "./vodObject";
+import VodDo from "./objects/vodDo";
+import VodIf from "./objects/vodIf";
+import VodObject from "./vodObject";
 
-export type VodBody = VodObject<any, any, VodObjectContent<any>>[];
+export type VodBody = VodObject<any, any, VodBodyContent>[];
 
 export default VodBody;
+
+export type VodBodyContent = VodDo<any> | VodIf<any>
