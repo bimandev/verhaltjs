208a1aa98e47c9da401b7b1e95dd069b01f6a204
schamrockt <k4yr225@gmail.com>
2025-02-11 19:04:20 +0300

    Refactor Vod class methods to return VodObject types instead of arrays

--- lib/vod.ts
+++ lib/vod.ts
@@ -1,15 +1,22 @@
+import VodObject from "./vodObject";
 import VodBody from "./vodBody";
 import VodDo from "./objects/vodDo";
 import VodIf from "./objects/vodIf";
 
 export class Vod {
 
-    public static do<TValue>(path : string, value : TValue, modus? : string) : VodDo<TValue> {
-        return [path, value, modus] as VodDo<TValue> ;
+    public static do<TValue>(path : string, value : TValue, modus? : string) : VodObject<TValue> {
+        return {
+            type: "do",
+            content: [path, value, modus] as VodDo<TValue>
+        }
     }
 
-    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodIf<TValue> {
-        return [path, modus, value, body] as VodIf<TValue> ;
+    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodObject<TValue> {
+        return {
+            type: "if",
+            content: [path, modus, value, body] as VodIf<TValue>
+        }
     }
 }
 
