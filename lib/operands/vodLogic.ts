601fa9e82b5a847d9e13539bd7007afadc1e79fd
schamrockt <k4yr225@gmail.com>
2025-02-12 00:22:17 +0300

    Refactor VodLogic type to use VodLogicSymbol for improved operator representation

--- lib/operands/vodLogic.ts
+++ lib/operands/vodLogic.ts
@@ -1,5 +1,11 @@
 import VodCond from "./vodCond";
 
-export type VodLogic = [VodCond, string, VodCond];
+export type VodLogic = [VodCond, VodLogicSymbol, VodCond];
 
 export default VodLogic;
+
+export type VodLogicSymbol = VodLogicAndSymbol | VodLogicOrSymbol;
+
+export type VodLogicAndSymbol = "&&";
+
+export type VodLogicOrSymbol = "||";
