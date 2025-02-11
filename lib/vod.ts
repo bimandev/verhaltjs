d4495d45929039106bbfe020689d5d665567ab9f
schamrockt <k4yr225@gmail.com>
2025-02-11 18:56:52 +0300

    Add static do method to Vod class for creating VodDo instances

--- lib/vod.ts
+++ lib/vod.ts
@@ -1,7 +1,12 @@
-import { VodIf } from "./objects/vodIf";
 import VodBody from "./vodBody";
+import VodDo from "./objects/vodDo";
+import VodIf from "./objects/vodIf";
 
 export class Vod {
+
+    public static do<TValue>(path : string, value : TValue, modus? : string) : VodDo<TValue> {
+        return [path, value, modus] as VodDo<TValue> ;
+    }
 
     public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodIf<TValue> {
         return [path, modus, value, body] as VodIf<TValue> ;
