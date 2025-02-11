6bcf95ecde44bc1f87b492c173e862ad2e7afe08
schamrockt <k4yr225@gmail.com>
2025-02-11 19:23:21 +0300

    Refactor VodIfOther type to utilize VodObject for enhanced type safety and flexibility

--- lib/objects/vodIf.ts
+++ lib/objects/vodIf.ts
@@ -1,3 +1,4 @@
+import VodObject from "../vodObject";
 import VodBody from "../vodBody";
 import VodElse from "./vodElse";
 import VodElseIf from "./vodElseIf";
@@ -6,4 +7,4 @@ export type VodIf<TValue> = [string, string, TValue, VodBody, VodIfOther<TValue>
 
 export default VodIf;
 
-export type VodIfOther<TValue> = VodElseIf<TValue> | VodElse<TValue>;
+export type VodIfOther<TValue> = VodObject<TValue, VodElseIf<TValue>> | VodObject<TValue, VodElse<TValue>>;
