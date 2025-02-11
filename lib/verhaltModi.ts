d02e9f1e012b6bc7e6d3828b30b03a633c568150
schamrockt <k4yr225@gmail.com>
2025-02-11 08:41:41 +0300

    Enhance VerhaltModi class to support dynamic operation mapping and generic types

--- lib/verhaltModi.ts
+++ lib/verhaltModi.ts
@@ -1,15 +1,34 @@
 import { VerhaltModus } from "./verhaltModus";
 
-export class VerhaltModi {
+export class VerhaltModi<TValue> {
+    #map : Map<string, VerhaltModus<TValue>> = new Map();
+
+    constructor(modi? : [string, VerhaltModus<TValue>][]) {
+        if (typeof (undefined as any as TValue) === "number") {
+            this.#map.set("add", VerhaltModi.numberAdd as unknown as VerhaltModus<TValue>);
+            this.#map.set("sub", VerhaltModi.numberSub as unknown as VerhaltModus<TValue>);
+            this.#map.set("mul", VerhaltModi.numberMul as unknown as VerhaltModus<TValue>);
+            this.#map.set("div", VerhaltModi.numberDiv as unknown as VerhaltModus<TValue>);
+        }
+
+        if(modi !== undefined) {
+            for (const [key, value] of modi) {
+                this.#map.set(key, value);
+            }
+        }
+    }
+    
+    //
+    
     public static numberAdd : VerhaltModus<number> 
-    = new VerhaltModus<number>("add", (prev, value) => prev + value);
+    = new VerhaltModus<number>((prev, value) => prev + value);
 
     public static numberSub : VerhaltModus<number>
-    = new VerhaltModus<number>("sub", (prev, value) => prev - value);
+    = new VerhaltModus<number>((prev, value) => prev - value);
 
     public static numberMul : VerhaltModus<number>
-    = new VerhaltModus<number>("mul", (prev, value) => prev * value);
+    = new VerhaltModus<number>((prev, value) => prev * value);
 
     public static numberDiv : VerhaltModus<number>
-    = new VerhaltModus<number>("div", (prev, value) => prev / value);
+    = new VerhaltModus<number>((prev, value) => prev / value);
 }
