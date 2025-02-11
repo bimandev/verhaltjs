af827d28575f8f2a9a6eb85529062137ff36fa67
schamrockt <k4yr225@gmail.com>
2025-02-11 23:42:28 +0300

    Enhance VodArith type by introducing VodArithSymbol for improved operator representation

--- lib/operands/vodArith.ts
+++ lib/operands/vodArith.ts
@@ -1,5 +1,25 @@
 import VodValue from "../vodValue";
 
-export type VodArith = [VodValue, string, VodValue];
+export type VodArith = [VodValue, VodArithSymbol, VodValue];
 
-export default VodArith;;
+export default VodArith;
+
+export type VodArithSymbol = 
+    VodArithAddSymbol | 
+    VodArithSubSymbol | 
+    VodArithMulSymbol | 
+    VodArithDivSymbol | 
+    VodArithModSymbol | 
+    VodArithPowSymbol;
+
+export type VodArithAddSymbol = "+";
+
+export type VodArithSubSymbol = "-";
+
+export type VodArithMulSymbol = "*";
+
+export type VodArithDivSymbol = "/";
+
+export type VodArithModSymbol = "%";
+
+export type VodArithPowSymbol = "^";
