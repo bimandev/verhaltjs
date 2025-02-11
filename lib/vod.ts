ca8cc42dd83622dae7273b65f9cce30c56c03095
schamrockt <k4yr225@gmail.com>
2025-02-11 18:52:30 +0300

    Add static if method to Vod class for conditional logic

--- lib/vod.ts
+++ lib/vod.ts
@@ -1,5 +1,11 @@
+import { VodIf } from "./objects/vodIf";
+import VodBody from "./vodBody";
+
 export class Vod {
 
+    public static if<TValue>(path : string, modus : string, value : TValue, body : VodBody) : VodIf<TValue> {
+        return [path, modus, value, body] as VodIf<TValue> ;
+    }
 }
 
 export default Vod;
