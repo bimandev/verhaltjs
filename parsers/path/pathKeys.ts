8c1b88f94eef06403bcd0800906b3be0f0b8f912
schamrockt <kyrzkaya@gmail.com>
2025-02-13 01:48:51 +0300

    Add pathKeysParser function for parsing input strings with brackets and dots

---
+++ parsers/path/pathKeys.ts
@@ -0,0 +1,54 @@
+export function pathKeysParser(input? : string) : string[] {
+    if(input === undefined) {
+        return [];
+    }
+
+    const values : string[] = []; // :haha[]
+    let record : string[] = [];
+    let depth : number = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        const char = input[i];
+        let disable = false;
+
+        switch (char) {
+            case "[" : {
+                depth++;
+                break;
+            }
+            case "]" : {
+                if(depth === 0) {
+                    throw new Error();
+                }
+
+                depth--;
+                break;
+            }
+            case " " : {
+                if(depth !== 0) {
+                    break;
+                }
+
+                throw new Error();
+            }
+            case "." : {
+                if(depth !== 0) {
+                    break;
+                }
+
+                values.push(record.join(""));
+                record = [];
+                disable = true;
+                break;
+            }
+        }
+
+        if(!disable)
+            record.push(char);
+    }
+
+    values.push(record.join(""));
+    record = [];
+
+    return values;
+}
