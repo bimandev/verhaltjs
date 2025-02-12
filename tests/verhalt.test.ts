c76579791d86880bc17fa0f91124ea2102e99b94
schamrockt <k4yr225@gmail.com>
2025-02-12 05:06:20 +0300

    Remove unused Verhalt classes and related types to streamline the codebase

--- tests/verhalt.test.ts
+++ tests/verhalt.test.ts
@@ -1,34 +1,3 @@
-import { Verhalt } from "../lib/verhalt";
-
-export type ExampleModel = {
-    money: number;
-    user: {
-        name: string;
-        age: number;
-    };
-};
-
-const model: ExampleModel = {
-    money: 100,
-    user: {
-        name: 'John',
-        age: 30
-    }
-};
-
 test('Verhalt.update', () => {
-    Verhalt.en(model)
-        .do(["user.age", 31])
-        .do(["money", 31, "+"])
-        .do(["user.age", { first: 12, last : 35}, "*"])
-        .do(["user.age.first", 35, "*"])
-        .do(["user.age.first", 2, "/"])
-        .do(["user.name", "Karamelto"]);
 
-    console.log(JSON.stringify(model, null, 2), typeof model.user.age);
-
-    // V.if(model, ["money", ">", 100])
-    //      .then("user.age", 31, "+")
-    //          .en("user.age", 31, "+")
-    //      .else("user.age", 31, "-");
 });
