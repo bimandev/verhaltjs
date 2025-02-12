038f17eba0e057148bddef3b8d0c941b139571e8
schamrockt <k4yr225@gmail.com>
2025-02-12 17:31:15 +0300

    Refactor keyIndex function to simplify input validation and improve return logic

--- regex/key/keyIndex.ts
+++ regex/key/keyIndex.ts
@@ -1,24 +1,13 @@
 import { Verhalt } from "../../lib/verhalt";
 import { VerhaltKeyIndex } from "../../lib/verhaltKey";
 import VerhaltModel from "../../lib/verhaltModel";
-import { pathKeysRegex } from "../path/pathKeys";
+import { pathKeys, pathKeysRegex } from "../path/pathKeys";
 
-export const keyIndexRegex = new RegExp(`^(?:${pathKeysRegex.source}|^\\d+$)$`);
 
 export function keyIndex(model : VerhaltModel, input: string) : VerhaltKeyIndex {
-    const match = input?.match(keyIndexRegex) ?? undefined;
-
-    if (match) {
-        let index = Number(input);
-        if(Number.isInteger(index)) {
-            return index;
-        }
-
-        const value = Verhalt.value(model, input);
-        if(Number.isInteger(value)) {
-            return value as number;
-        }
+    if(/^[1-9]\d*$/.test(input)) {
+        return parseInt(input);
     }
 
-    return undefined;
+    return Verhalt.value(model, input) as number;
 }
