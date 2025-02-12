1a9852b44aa799822ee8ca0225a0fd09b4ff5113
schamrockt <k4yr225@gmail.com>
2025-02-12 06:11:57 +0300

    Implement value method in Verhalt class for enhanced path validation and error handling

--- lib/verhalt.ts
+++ lib/verhalt.ts
@@ -1,4 +1,34 @@
+import VerhaltModel from "./verhaltModel";
+import VerhaltPath, { verhaltPathRegex } from "./verhaltPath";
+import { VerhaltValue } from "./verhaltValue";
 
 export class Verhalt {
-    
+    public static value<TModel extends VerhaltModel>(model: TModel, path: VerhaltPath): VerhaltValue { 
+        const pathMatch = path.match(verhaltPathRegex);
+
+        if (pathMatch) {
+            const keys = pathMatch.slice(1);
+            let target = model;
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i];
+                const isLast = i === keys.length - 1;
+
+                target = target[key];
+
+                if(isLast) {
+                    return target;
+                }
+
+                if (target === null || typeof target !== "object") {
+                    return "?VERHALT_PATH_INCOMPLETE";
+                }
+
+                if (target[key] === undefined) {
+                    return "?VERHALT_PATH_INVALID";
+                }
+            }
+        }
+
+        return "?VERHALT_PATH_INVALID";
+    }
 }
