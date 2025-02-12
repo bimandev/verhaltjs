935d23a7e96c88a265cf12986c16d4b3f05bb305
schamrockt <k4yr225@gmail.com>
2025-02-12 23:14:56 +0300

    Add rootGroup function to extract matched groups from input

--- regex/path/pathGroups.ts
+++ regex/path/pathGroups.ts
@@ -1,3 +1,12 @@
 export const rootGroupRegex = /(?:^:(\S*))|(..*)/g
 
 export const optionalGroupsRegex = /(?:\s*\?\?\s(\S+))/g
+
+export function rootGroup(input: string) : [string, string] | undefined {
+    const match = input.match(rootGroupRegex)
+    if (match) {
+        return [match[1], match[2]]
+    }
+
+    return undefined;
+}
