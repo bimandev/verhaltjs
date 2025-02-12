f343c8e90f17d2fbb4a204d94fe399be7949e521
schamrockt <k4yr225@gmail.com>
2025-02-12 23:17:22 +0300

    Fix rootGroup function to return the full match and first capturing group

--- regex/path/pathGroups.ts
+++ regex/path/pathGroups.ts
@@ -5,7 +5,7 @@ export const optionalGroupsRegex = /(?:\s*\?\?\s(\S+))/g
 export function rootGroup(input: string) : [string, string] | undefined {
     const match = input.match(rootGroupRegex)
     if (match) {
-        return [match[1], match[2]]
+        return [match[0], match[1]]
     }
 
     return undefined;
