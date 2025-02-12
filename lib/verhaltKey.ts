4a753f37147b7540e4f834fe331e32f104f81b77
schamrockt <k4yr225@gmail.com>
2025-02-12 08:39:46 +0300

    Add VerhaltKey type definition for improved type structure

---
+++ lib/verhaltKey.ts
@@ -0,0 +1,7 @@
+export type VerhaltKey = [VerhaltKeyName, VerhaltKeyContent];
+
+export default VerhaltKey;
+
+export type VerhaltKeyName = string;
+
+export type VerhaltKeyContent = string | undefined;
