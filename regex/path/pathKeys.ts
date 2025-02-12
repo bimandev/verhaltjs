f3b72ff31573fadf3239fe873cc3f01134971f9b
schamrockt <k4yr225@gmail.com>
2025-02-12 08:38:16 +0300

    Refactor pathKeys function to accept VerhaltPath type for improved type safety

--- regex/path/pathKeys.ts
+++ regex/path/pathKeys.ts
@@ -1,6 +1,8 @@
+import VerhaltPath from "../../lib/verhaltPath";
+
 export const pathKeysRegex = /^:([a-zA-Z][a-zA-Z0-9]*)(?:\[[^\]]*\])?(?:\.[a-zA-Z][a-zA-Z0-9]*(?:\[[^\]]*\])?)*$/;
 
-export function pathKeys(path: string) : string[] {
+export function pathKeys(path: VerhaltPath) : string[] {
     const match = path.match(pathKeysRegex);
 
     if (!match) {
