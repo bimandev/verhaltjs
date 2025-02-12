cfbd27cc3db3a3d6e5ec7ad615668061e3135f22
schamrockt <k4yr225@gmail.com>
2025-02-12 17:21:15 +0300

    Refactor pathKeys function to improve error handling and enhance path validation logic

--- regex/path/pathKeys.ts
+++ regex/path/pathKeys.ts
@@ -3,15 +3,84 @@ import VerhaltPath from "../../lib/verhaltPath";
 export const pathKeysRegex = /(:[^.]+)|(\.[^.]+)/g;
 
 export function pathKeys(path: VerhaltPath) : string[] {
-    if(path === undefined || path[0] !== ":") {
-        return [];
+    if (path === undefined || path[0] !== ":") {
+        throw new Error("Path should start with ':' character");
     }
 
-    const match = path?.match(pathKeysRegex) ?? undefined;
+    const keys: string[] = [];
+    let current: string[] = [];
+    let cursor: number = 0;
+    let depth: number = 0;
+    let mode: PathKeysMode = "root";
 
-    if (!match) {
-        return [];
+    for (let i = 0; i < path.length; i++) {
+        const char = path[i];
+
+        switch (char) {
+            case ":": {
+                if (i === 0) {
+                    mode = "root";
+                    cursor = -1;
+                } else if (depth === 0) {
+                    throw new Error("':' character is not allowed in the middle of the path");
+                }
+                break;
+            }
+            case ".": {
+                if (depth === 0) {
+                    mode = "group";
+                    cursor = -1;
+                }
+                break;
+            }
+            case "[": {
+                depth++;
+                break;
+            }
+            case "]": {
+                if(depth === 0) {
+                    throw new Error("Mismatch '[' character in path");
+                }
+                
+                depth--;
+                break;
+            }
+            default: {
+                if (!/[a-zA-Z0-9]/.test(char)) {
+                    throw new Error("Invalid character in path");
+                }
+
+                if (cursor === 0) {
+                    if (/[0-9]/.test(char)) {
+                        throw new Error("Key should not start with a number");
+                    }
+                }
+            }
+        }
+
+        if (cursor === -1) {
+            if (mode === "group") {
+                keys.push(current.join(""));
+                current = [];
+            }
+        } else {
+            current.push(char);
+        }
+
+        cursor++;
     }
 
-    return match.slice(0).map((x) => x.slice(1));
+    if(depth !== 0) {
+        throw new Error("Mismatch ']' character in path");
+    }
+
+    keys.push(current.join(""));
+    current = [];
+    return keys;
 }
+
+export type PathKeysMode = PathKeysModeRoot | PathKeysModeGroup;
+
+export type PathKeysModeRoot = "root";
+
+export type PathKeysModeGroup = "group";
