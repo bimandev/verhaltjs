28b97edbcf8a4d9195838fc0c9db16d9a66851be
schamrockt <k4yr225@gmail.com>
2025-02-12 19:33:45 +0300

    Enhance keyContent function to support nullable parameter in regex match

--- regex/key/keyContent.ts
+++ regex/key/keyContent.ts
@@ -2,17 +2,19 @@ import VerhaltKey, { VerhaltKeyContent } from "../../lib/verhaltKey";
 import VerhaltModel from "../../lib/verhaltModel";
 import { keyIndex } from "./keyIndex";
 
-const regex = /^([a-zA-Z][a-zA-Z0-9]*)?(?:\[(.*?)]$)?/;
+const regex = /^([a-zA-Z][a-zA-Z0-9]*)?(?:\[([a-zA-Z0-9]*)\])?(\?)?$/;
 
 export function keyContent(model : VerhaltModel, input : string) : VerhaltKeyContent {
     const match = input?.match(regex) ?? undefined;
     
     if (match) {
         const name = match[1];
-        const index = match[2];
+        const index = match[2];        
+        const nullable = match[3];
 
-        return [name, index ? keyIndex(model, index) ?? undefined : undefined];
+
+        return [name, index ? keyIndex(model, index) ?? undefined : undefined, nullable ? true : false];
     }
 
-    return [undefined, undefined];
+    return [undefined, undefined, undefined];
 }
