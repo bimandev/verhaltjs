aeb4fd898a505f7df0590fee202553a5b07e39d0
schamrockt <kyrzkaya@gmail.com>
2025-02-13 03:39:07 +0300

    Add static keyContentParser method to VerhaltPath for enhanced input parsing

--- lib/verhaltPath.ts
+++ lib/verhaltPath.ts
@@ -1,7 +1,12 @@
+import { pathKeyContentParser } from "../parsers/path/pathKeyContent";
 import { pathKeysParser } from "../parsers/path/pathKeys";
 import { pathValuesParser } from "../parsers/path/pathValues";
 
 export class VerhaltPath {
+    static keyContentParser(input? : string) {
+        return pathKeyContentParser(input);
+    }
+
     static keysParser(input? : string) : string[] {
         return pathKeysParser(input);
     }
