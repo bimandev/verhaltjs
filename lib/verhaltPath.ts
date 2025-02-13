ab66824feaa0abdb1868aca460883c782e265ba3
schamrockt <kyrzkaya@gmail.com>
2025-02-13 03:35:14 +0300

    Refactor VerhaltPath to a class structure for improved type safety and extensibility

--- lib/verhaltPath.ts
+++ lib/verhaltPath.ts
@@ -1,11 +1,5 @@
-export type VerhaltPath = string;
+export class VerhaltPath {
+
+}
 
 export default VerhaltPath;
-
-export type VerhaltPathError = VerhaltPathErrorInvalid | VerhaltPathErrorNotExists;
-
-export type VerhaltPathErrorInvalid = "?VERHALT_PATH_INVALID";
-
-export type VerhaltPathErrorIncomplete = "?VERHALT_PATH_INCOMPLETE";
-
-export type VerhaltPathErrorNotExists = "?VERHALT_PATH_NOT_EXISTS";
