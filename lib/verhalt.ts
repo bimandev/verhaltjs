01f546bf830e7753657fb7b3a4331f7f9928677d
schamrockt <kyrzkaya@gmail.com>
2025-02-13 04:26:46 +0300

    Enhance Verhalt.value method to accept an optional path parameter and implement path value retrieval logic

--- lib/verhalt.ts
+++ lib/verhalt.ts
@@ -1,8 +1,39 @@
 import VerhaltValue from "./core/verhaltValue";
+import VerhaltParser from "./verhaltParser";
 
 export class Verhalt {
-    public static value() : VerhaltValue {
+    public static value(path? : string) : VerhaltValue {
+        if(path === undefined) {
+            return undefined;
+        }
 
+        const pathValues = VerhaltParser.pathValues();
+        for(const pathWay of pathValues) {
+            try {
+                return Verhalt.valueFromWay(pathWay);
+            }
+            catch(error) {
+                if(pathValues[pathValues.length - 1] === pathWay) {
+                    throw error;
+                }
+
+                continue;
+            }
+        }
+    }
+
+    private static valueFromWay(way? : string) : VerhaltValue {
+        if(way === undefined) {
+            return undefined;
+        }
+
+        const keys = VerhaltParser.pathKeys(way);
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            
+            const content = VerhaltParser.pathKeyContent(key);
+            console.log(content);
+        }
     }
 }
 
