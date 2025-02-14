48f97562a24a28d14ad885149f66fea8b3bfe73d
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 03:12:12 +0300

    Update test cases in verhalt.test.ts to include new example model and enhance path key testing

--- tests/verhalt.test.ts
+++ tests/verhalt.test.ts
@@ -1,3 +1,4 @@
+import { keyContent, pathKeys, routePaths } from "@verhalt/parser";
 import Verhalt from "../lib/verhalt";
 import { test } from 'vitest';
 
@@ -13,9 +14,9 @@ type ExampleModel = {
     lib: any[];
 }
 
-const exampleModel : ExampleModel = {
+const exampleModel = [{
     cigu: [
-        2, 0, {nina: 1}
+        2, 0, {nina: "as"}
     ],
     money : 100,
     age : 20,
@@ -23,11 +24,15 @@ const exampleModel : ExampleModel = {
         first : "John",
         last : "Doe"
     },
-    lib: ["a", { haha: "woho"}, "c"]
-}
+    lib: ["a", { haha: "woho"}, "c"],
+    shihi: [[], [["a", "b"]], ["c", "d"]]
+}]
 
 test('Verhalt.value', () => {
     //const values = VerhaltParser.pathValues(":cigu[0]? ?? :money[:age ?? 0]? ?? :name[first] ?? :lib[:lib[1].a ?? 0]?.haha? ?? :cigu[2].nina");
     //const keys = VerhaltParser.pathKeyContent("lib[:age ?? 0]?[4]?[dsf][0]?[:cigu[0]? ?? :money[:age ?? 0]? ?? :name[first] ?? :lib[:lib[1].a ?? 0]?.haha? ?? :cigu[2].nina]?[dfsd]");
-    console.log(Verhalt.value("lib[0][234][54]?.aha.hihi[1]")); 
+    const path = routePaths(":cigu[0]?")[0];
+    console.log(routePaths(":cigu[0] ?? :money ?? :age ?? :name[first] ?? :lib[:lib[1].a ?? 0]?.haha ?? :cigu[2].nina"));
+
+    console.log(Verhalt.model(exampleModel, ":[0].shihi[1][0][1]"));
 });
