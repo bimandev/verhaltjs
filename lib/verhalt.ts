3a42285090b9778be0f207c9c59f7164b78c2468
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 05:02:20 +0300

    Refactor Verhalt class to simplify path resolution by introducing fromPath function and removing resolve_path method

--- lib/verhalt.ts
+++ lib/verhalt.ts
@@ -3,10 +3,6 @@ import { VerhaltArrayModel, VerhaltModel, VerhaltObjectModel, VerhaltStructureMo
 import { pathError } from "./utils/error";
 
 export class Verhalt {
-    public static lookup<TModel extends VerhaltObjectModel>(source : TModel, route : string) : VerhaltModel {
-        return Verhalt.resolve(source, route);
-    }
-
     private static resolve<TModel extends VerhaltObjectModel>(source : TModel, route? : string) : VerhaltModel {
         if(route === undefined) {
             return undefined;
@@ -16,7 +12,7 @@ export class Verhalt {
 
         for(const path of paths) {
             try {                
-                return Verhalt.resolve_path(source, path);
+                return fromPath(path);
             }
             catch(error) {
                 if(paths[paths.length - 1] === path) {
@@ -26,75 +22,79 @@ export class Verhalt {
                 continue;
             }
         }
+
+        function fromPath(path? : string) {
+            if(path === undefined) {
+                return undefined;
+            }
+    
+            let current = source;
+            const keys = pathKeys(path);
+            const completedKeys = [];
+    
+            for (let i = 0; i < keys.length - 1; i++) {
+                const key = keys[i];
+                completedKeys.push(key);
+    
+                const [head, body] = keyContent(key);
+                const [headNull, headName] = head ?? [false, undefined];
+    
+                if(headName) {
+                    if(typeof current !== "object") {
+                        throw pathError(completedKeys, `Expected object, got ${typeof current}`);
+                    }
+    
+                    if(Array.isArray(current)) {
+                        throw pathError(completedKeys, `Expected object, got array`);
+                    }
+    
+                    if(!(headName in (current as object))) {
+                        throw pathError(completedKeys, `Expected key ${headName} in object`);
+                    }
+    
+                    current = (current as VerhaltStructureModel)[headName];
+                }
+    
+                if(body) {
+                    for(const [contentNull, contentValue] of body) {
+                        if(!Array.isArray(current)) {
+                            throw pathError(completedKeys, `Expected array, got ${typeof current}`);
+                        }
+    
+                        let index = keyIndex(contentValue);
+    
+                        if(index === null) {
+                            throw pathError(completedKeys, `Expected index number, got ${typeof index}`);
+                        }
+    
+                        if(typeof index === "string") {
+                            
+                            let value;
+                            try {
+                                value = Verhalt.lookup(source, index);
+                            }
+                            catch(error) {
+                                throw pathError(completedKeys, `Error during model lookup. \error:${error}`);
+                            }
+    
+                            if(typeof value !== "number") {
+                                throw pathError(completedKeys, `Expected number, got ${typeof value}`);
+                            }
+    
+                            index = value;
+                        }
+    
+                        current = (current as VerhaltArrayModel)[index];
+                    }
+                }
+            }
+    
+            return current;
+        }
     }
 
-    private static resolve_path<TModel extends VerhaltObjectModel>(source : TModel, path? : string) : VerhaltModel {
-        if(path === undefined) {
-            return undefined;
-        }
-
-        let current = source;
-        const keys = pathKeys(path);
-        const completedKeys = [];
-
-        for (let i = 0; i < keys.length - 1; i++) {
-            const key = keys[i];
-            completedKeys.push(key);
-
-            const [head, body] = keyContent(key);
-            const [headNull, headName] = head ?? [false, undefined];
-
-            if(headName) {
-                if(typeof current !== "object") {
-                    throw pathError(completedKeys, `Expected object, got ${typeof current}`);
-                }
-
-                if(Array.isArray(current)) {
-                    throw pathError(completedKeys, `Expected object, got array`);
-                }
-
-                if(!(headName in (current as object))) {
-                    throw pathError(completedKeys, `Expected key ${headName} in object`);
-                }
-
-                current = (current as VerhaltStructureModel)[headName];
-            }
-
-            if(body) {
-                for(const [contentNull, contentValue] of body) {
-                    if(!Array.isArray(current)) {
-                        throw pathError(completedKeys, `Expected array, got ${typeof current}`);
-                    }
-
-                    let index = keyIndex(contentValue);
-
-                    if(index === null) {
-                        throw pathError(completedKeys, `Expected index number, got ${typeof index}`);
-                    }
-
-                    if(typeof index === "string") {
-                        
-                        let value;
-                        try {
-                            value = Verhalt.lookup(source, index);
-                        }
-                        catch(error) {
-                            throw pathError(completedKeys, `Error during model lookup. \error:${error}`);
-                        }
-
-                        if(typeof value !== "number") {
-                            throw pathError(completedKeys, `Expected number, got ${typeof value}`);
-                        }
-
-                        index = value;
-                    }
-
-                    current = (current as VerhaltArrayModel)[index];
-                }
-            }
-        }
-
-        return current;
+    public static lookup<TModel extends VerhaltObjectModel>(source : TModel, route : string) : VerhaltModel {
+        return Verhalt.resolve(source, route);
     }
 }
 
