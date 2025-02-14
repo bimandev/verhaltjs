8f3c8557385c3f8b94a1c850a5a0485e028a1ae3
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 03:52:16 +0300

    Refactor pathError function to support both string and array inputs for improved error messaging

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,6 +1,13 @@
-export function pathError(path : string, message : string) {
+export function pathError(path : string, message : string) : Error;
+export function pathError(keys : string[], message : string) : Error;
+export function pathError(source : string | string[], message : string) : Error
+export function pathError(source : string | string[], message : string) : Error {
+    if(!Array.isArray(source)) {
+        source = source.split("");
+    }
+
     return new Error(`
         Invalid path: ${message}
-        \npath: ${path}`
+        \npath: ${source}`
     );
 }
