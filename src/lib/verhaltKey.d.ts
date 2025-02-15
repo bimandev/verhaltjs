79c876283e1048fb3ad209ec2b305ae9fb58ff3e
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 01:34:23 +0300

    Bump version to 1.7.3 and add VerhaltForm type with root and extension variants

--- src/lib/verhaltKey.d.ts
+++ src/lib/verhaltKey.d.ts
@@ -1,9 +1,16 @@
 import { VerhaltStep } from "./verhaltStep";
 
 export type VerhaltKey = {
+    form : VerhaltForm
     steps : VerhaltKeySteps
     catching : VerhaltKeyCatching
 };
+
+export type VerhaltForm = VerhaltRootForm | VerhaltExtensionForm;
+
+export type VerhaltRootForm = "root";
+
+export type VerhaltExtensionForm = "extension";
 
 export type VerhaltKeySteps = VerhaltStep[];
 
