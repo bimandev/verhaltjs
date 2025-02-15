532c43318274c01882ebb96f8a4f866f1aad0db5
k4yr2 <kyrzkaya@gmail.com>
2025-02-15 12:21:06 +0300

    Refactor parseKeyWithoutTokenUnsafe to utilize InputInfo for character processing

--- src/lib/key/parseKey.ts
+++ src/lib/key/parseKey.ts
@@ -1,5 +1,7 @@
-import { VerhaltKey, VerhaltKeyHead, VerhaltKeyBody, VerhaltKeyItem } from "@verhalt/types/lib";
+import { VerhaltKey } from "@verhalt/types/lib";
 import { checkKey, checkKeyWithoutToken } from "./checkKey";
+import { InputInfo } from "../inputInfo";
+import { CharInfo } from "../charInfo";
 
 export function parseKey(input: string) : VerhaltKey | undefined {
     checkKey(input);
@@ -18,161 +20,24 @@ export function parseKeyWithoutToken(input: string) : VerhaltKey | undefined {
 }
 
 export function parseKeyWithoutTokenUnsafe(input: string, isRoot : boolean = false) : VerhaltKey | undefined {
-    if (!input) return undefined;
+    if(!input) return undefined;
 
-    let nameBuffer: string[] = [];
-    let depthBuffer: string[] = [];
+    let info = new InputInfo(input);
+    do {
+        const char = info.current as CharInfo;
 
-    let name : string | null | undefined = undefined;
-    let depth = 0;
-    let charIndex = 0;
-    let charIndexNullable = -1;
-    let globalNullableIndex = -1;
-
-    const bodyName : VerhaltKeyItem = { value: "", nullable: false, dynamic: false };
-    const bodyIndexes : VerhaltKeyItem[] = [];
-    
-    const head: VerhaltKeyHead = { silent: false }
-    const body: VerhaltKeyBody = { name: bodyName, indexes: bodyIndexes }
-
-    if(input.slice(input.length - 2, input.length) === "??") {
-        head.silent = true;
-        globalNullableIndex = input.length - 1;
-    }
-
-
-    for (charIndex = 0; charIndex < input.length; charIndex++) {
-        const char = input[charIndex];
-
-        if (name === undefined) {
-            handleHeadName(char);
-        }
-
-        switch (char) {
-            case '[':
-                handleOpenBracket();
-                break;
-            case ']':
-                handleCloseBracket();
-                break;
-            case '?':
-                handleNullable();
-                break;
-        }
-
-        handleCharacter(char);
-    }
-
-    if (depth !== 0) throw new Error("[VERHALT-KEY]: Square brackets are not balanced.");
-    if (name === undefined) name = nameBuffer.join("");
-
-    body.name.value = name;
-    return { head, body };
-
-    function handleHeadName(char: string) {
-        switch(char) {
-            case "?" : {
-                if(nameBuffer.length === 0) {
-                    throw new Error("[VERHALT-KEY]: Key cannot start with '?' character.");
-                }
-                charIndexNullable = charIndex;
-            }
-            case "[" : {
-                if (nameBuffer.length === 0)  {
+        if(info.cursor === 0) {
+            if(!char.isAlphabetic) {
+                if(char.isCrulyOpenBracket || char.isSquareOpenBracket) {
                     if(!isRoot) {
-                        throw new Error("[VERHALT-KEY]: Non-root key must start with a name.");
-                    }
-
-                    name = null;
+                        throw new Error("[VERHALT-KEY]: It must start with alphabetic character.");
+                    }            
                 }
                 else {
-                    name = nameBuffer.join("");
-                }
-                break;
-            }
-        }
-    }
-
-    function handleOpenBracket() {
-        if (depth === 0) {
-            body.indexes.push({ value: "", nullable: false, dynamic: false });
-            depthBuffer = [];
-        }
-        depth++;
-    }
-
-    function handleCloseBracket() {
-        if (depth === 0) throw new Error("[VERHALT-KEY]:Square brackets are not balanced.");
-        
-        if (depth === 1) {
-            if(depthBuffer.length === 0) {
-                throw new Error("[VERHALT-KEY]: Key indexer must contain something.");
-            }
-
-            const current = body.indexes[body.indexes.length - 1];
-            current.value = depthBuffer.join("");
-
-            charIndexNullable = charIndex + 1;
-        }
-        depth--;
-    }
-
-    function handleNullable() {
-        if (depth !== 0) return;
-        if (charIndexNullable !== charIndex) {
-
-            if(charIndex !== globalNullableIndex) {
-                throw new Error("[VERHALT-KEY]:Invalid '?' character");
-            }
-        }
-
-        const current = body.indexes[body.indexes.length - 1];
-        if (current) {
-            current.nullable = true;
-        } else {
-            body.name.nullable = true;
-        }
-
-        charIndexNullable = -1;
-    }
-
-    function handleCharacter(char: string) {
-        if (name !== undefined) {
-            if (depth > 0) {
-                switch (char) {
-                    case "[": {
-                        if(depth === 1) {
-                            break;
-                        }
-                    }
-                    default: {
-                        depthBuffer.push(char);
-                    }
+                    throw new Error("[VERHALT-KEY]: It must start with alphabetic character.");
                 }
             }
-            else {
-                if(!/[\[\]\?]/.test(char)) {
-                    throw new Error("[VERHALT-KEY]: Key must just contain '[', ']' or '?' character after name.");
-                }
-            }
-        } else {
-            if (nameBuffer.length === 0) {
-                if(!/[a-zA-Z]/.test(char)) {
-                    switch (char) {
-                        case " ":
-                            throw new Error("[VERHALT-KEY]: Key must not start with white spaces.");
-                        default:
-                            throw new Error("[VERHALT-KEY]: Key must start with a letter or '[' character.");
-                    }
-                }
-            }
-            else {
-                if(!/[a-zA-Z0-9]/.test(char)) {
-                    throw new Error("[VERHALT-KEY]: Key name must contain letters or numbers.");
-                }
-            }
-
-            nameBuffer.push(char);
         }
-    }
+
+    } while(info.next());
 }
