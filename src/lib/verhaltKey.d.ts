0cd293b78d0d1f45a34af916f9b7a4890acad972
k4yr2 <kyrzkaya@gmail.com>
2025-02-15 05:38:02 +0300

    Refactor VerhaltKey type to replace name and indexes with steps for improved structure

--- src/lib/verhaltKey.d.ts
+++ src/lib/verhaltKey.d.ts
@@ -1,19 +1,13 @@
+import { VerhaltStep } from "./verhaltStep";
+
 export type VerhaltKey = {
     head : VerhaltKeyHead
     body: VerhaltKeyBody 
 };
-
-export type VerhaltKeyItem = {
-    value: string | number
-    dynamic: boolean
-    nullable: boolean
-};
-
 export type VerhaltKeyHead = { 
     silent : boolean
 };
 
 export type VerhaltKeyBody = { 
-    name : VerhaltKeyItem
-    indexes : VerhaltKeyItem[] 
+    steps : VerhaltStep[]
 };
