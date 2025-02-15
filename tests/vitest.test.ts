42f9ec3e1c858beb3d4e130e4753a0f2136aebfd
k4yr2 <kyrzkaya@gmail.com>
2025-02-15 11:54:48 +0300

    Update parseStep test cases to include new input scenarios

--- tests/vitest.test.ts
+++ tests/vitest.test.ts
@@ -6,12 +6,5 @@ test('keyContent', async (context) => {
     const {parseKey: keyValue} = await import('../src/lib/key/parseKey');
     const {parseStep } = await import('../src/lib/step/parseStep');
 
-    console.log(parseStep("aha"));
-    console.log(parseStep("aha?"));
-    console.log(parseStep("aha!"));
-    console.log(parseStep("{:cigu[:cigu[0]]}!"));
-    console.log(parseStep("{:{:cigu[12]}.sasa}?"));
-    console.log(parseStep("{124}!"));
-    console.log(parseStep("[12]?"));
-    console.log(parseStep("[haha]!"));
+    console.log(parseStep("[:{ciguli}[:{mama}].haha]>?"));
 });
