d85a71c461badb4d2d7b09b2db2c3b52a26aabed
k4yr2 <kyrzkaya@gmail.com>
2025-02-15 12:08:37 +0300

    Add curly and square bracket counting to next() method in InputInfo class

--- src/lib/inputInfo.ts
+++ src/lib/inputInfo.ts
@@ -4,6 +4,8 @@ export class InputInfo {
     #input : string;
     #cursor : number = 0;
     #current : CharInfo | undefined ;
+    #curlyCount : number = 0;
+    #squareCount : number = 0;
 
     constructor(input : string) {
         if(typeof input !== "string") throw new Error("[VERHALT-INPUTINFO]: Input must be string");
@@ -27,12 +29,27 @@ export class InputInfo {
 
     public next() : CharInfo | undefined {
         this.#cursor++;
-        
+
         if(this.#cursor >= this.#input.length){
             this.#current = undefined;
         }
         else {
             this.#current = new CharInfo(this.#input[this.#cursor]);
+
+            switch(this.#current.target) {
+                case "{":
+                    this.#curlyCount++;
+                    break;
+                case "}":
+                    this.#curlyCount--;
+                    break;
+                case "[":
+                    this.#squareCount++;
+                    break;
+                case "]":
+                    this.#squareCount--;
+                    break;
+            }
         }
 
         return this.#current;
