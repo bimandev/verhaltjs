4347a68702b06cc4c7f8b9723bba503894e72beb
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 02:15:20 +0300

    Refactor parseRoutePaths to improve bracket handling and streamline path extraction logic

--- src/lib/route/parseRoutePaths.ts
+++ src/lib/route/parseRoutePaths.ts
@@ -1,48 +1,42 @@
+import { CharInfo } from "../charInfo";
+import { InputInfo } from "../inputInfo";
+
 export function parseRoutePaths(input?: string): string[] {
     if (!input) {
         return [];
     }
 
+    const head = "VERHALT-ROUTE-PATHS";
+    const info = new InputInfo(input);
     const paths: string[] = [];
-    let currentPath: string[] = [];
-    let depth = 0;
+    let pathBuffer: string[] = [];
+    
+    do {        
+        info.checkCurlyClose(head);
+        info.checkSquareClose(head);
 
-    for (let i = 0; i < input.length; i++) {
-        const char = input[i];
-        let skipChar = false;
+        const char = info.current as CharInfo;
+        pathBuffer.push(char.target);
 
-        if (char === "[") {
-            depth++;
-        }
-        else if (char === "]") {
-            if (depth === 0) {
-                throw new Error("Invalid bracket structure.");
-            }
-            depth--;
-        }
-        else if (char === " ") {
-            if (depth !== 0) {
-                continue;
-            }
+        if(char.isWhitespace) {
+            if(!(info.curlyStack !== 0 || info.squareStack !== 0)) {
+                if (input.slice(info.cursor, info.cursor + 4) === " ?? ") {
+                    info.skip(3);
+                    pathBuffer.pop();
 
-            if (input.slice(i, i + 4) === " ?? ") {
-                i += 3;
-                paths.push(currentPath.join(""));
-                currentPath = [];
-                skipChar = true;
-            } else {
-                throw new Error("Unexpected character or structure.");
+                    paths.push(pathBuffer.join(""));
+                    pathBuffer = [];
+                } else {
+                    throw new Error("Unexpected character or structure.");
+                }
             }
         }
 
-        if (!skipChar) {
-            currentPath.push(char);
-        }
-    }
 
-    if (currentPath.length > 0) {
-        paths.push(currentPath.join(""));
-    }
+    } while(info.next());
+
+    info.checkCurlyOpen(head);
+    info.checkSquareOpen(head);
 
     return paths;
 }
