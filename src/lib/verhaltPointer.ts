bb99b555a5463d7c64197f92ee5e03b22591b337
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 04:06:18 +0300

    Add key property to VerhaltPointer class and update constructor to accept VerhaltKey

--- src/lib/verhaltPointer.ts
+++ src/lib/verhaltPointer.ts
@@ -1,9 +1,20 @@
+import { VerhaltKey } from "./verhaltKey";
 import { VerhaltReference } from "./verhaltReference";
 
 export class VerhaltPointer {
+    private _key : VerhaltKey;
     private _refs : VerhaltReference[];
     
-    constructor(refs : VerhaltReference[]) {
+    constructor(key : VerhaltKey, refs : VerhaltReference[]) {
+        this._key = key;
         this._refs = refs;
+    }
+
+    public get key() : VerhaltKey {
+        return this._key;
+    }
+
+    public get refs() : ReadonlyArray<VerhaltReference> {
+        return this._refs;
     }
 }
