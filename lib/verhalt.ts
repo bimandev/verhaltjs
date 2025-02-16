3fbc09dab8eeb109aca5c76f9e963ab3c27f178d
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 12:30:27 +0300

    Enhance verhalt class to utilize VerhaltReferenceFlag for improved target assignment and update @verhalt/types dependency to version 1.8.8

--- lib/verhalt.ts
+++ lib/verhalt.ts
@@ -1,9 +1,9 @@
 import { parsePathKeys, parseKey } from "@verhalt/parser/lib"
-import { VerhaltArrayObject, VerhaltKey, VerhaltLink, VerhaltLinkOptions, VerhaltModel, VerhaltObjectModel, VerhaltPointer, VerhaltReference, VerhaltStep, VerhaltStructureObject } from "@verhalt/types/lib";
+import { VerhaltArrayObject, VerhaltKey, VerhaltLink, VerhaltLinkOptions, VerhaltModel, VerhaltObjectModel, VerhaltPointer, VerhaltReference, VerhaltReferenceFlag, VerhaltStep, VerhaltStructureObject } from "@verhalt/types/lib";
 
 export class verhalt {
     public static link(model : VerhaltObjectModel, path : string, options? : VerhaltLinkOptions) : VerhaltLink {
-        const origin : VerhaltReference = new VerhaltReference(null as unknown as VerhaltStep, ":", model);
+        const origin : VerhaltReference = new VerhaltReference(null as unknown as VerhaltStep, { value: ":" }, model);
         const pointers : VerhaltPointer[] = [];
 
         const keys = parsePathKeys(path);
@@ -17,34 +17,38 @@ export class verhalt {
                 if(typeof target !== "object") throw new Error("Invalid Target: target is not an object");
                 const isArray = Array.isArray(target);
 
-                let raw : any;
+                let flag : VerhaltReferenceFlag;
                 switch(step.structure) {
                     case "variable": {
-                        raw = verhalt.lookup(model, step.content as string); // ?: Implement variable structure
+                        const link = verhalt.link(model, step.content as string);
+                        flag = {
+                            link: link,
+                            value: link.current.obj as unknown as any
+                        }
                         break;
                     }
                     default:
                     case "static": {
-                        raw = step.content as string;
+                        flag = {
+                            link: undefined,
+                            value: step.content as string
+                        }
                         break;
                     }
                 }
 
-                let flag : string | number;
                 switch(step.form) {
                     case "name": {                        
                         if(isArray) throw new Error("Invalid Target: Target is an array");
 
-                        flag = raw;
-                        target = (target as VerhaltStructureObject)[flag]
+                        target = (target as VerhaltStructureObject)[flag.value as string]
                         break;
                     }
                     default:
                     case "index": {
                         if(!isArray) throw new Error("Invalid Target: Target is not an array");
 
-                        flag = Number(raw);
-                        target = (target as VerhaltArrayObject)[flag]
+                        target = (target as VerhaltArrayObject)[flag.value as number]
                         break;
                     }
                 }
@@ -66,11 +70,12 @@ export class verhalt {
     public static assign(model : VerhaltObjectModel, path : string, value : VerhaltModel) : void {
         const link = verhalt.link(model, path);
         const parent = link.parent!;
-
+        const flag = link.current.flag;
+        
         if(parent.step.form === "name")
-            (link.parent!.obj as VerhaltStructureObject)[link.current.flag as string] = value;
+            (link.parent!.obj as VerhaltStructureObject)[flag.value as string] = value;
         else
-            (link.parent!.obj as VerhaltArrayObject)[link.current.flag as number] = value
+            (link.parent!.obj as VerhaltArrayObject)[flag.value as number] = value
     }
 }
 
