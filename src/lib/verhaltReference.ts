99341732cd9772235936c6b0c0c8a185aaa678bf
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 05:26:39 +0300

    Add flag parameter to VerhaltReference constructor

--- src/lib/verhaltReference.ts
+++ src/lib/verhaltReference.ts
@@ -3,10 +3,12 @@ import { VerhaltStep } from "./verhaltStep";
 
 export class VerhaltReference {
     private _step : VerhaltStep
+    private _flag : string
     private _obj : VerhaltObjectModel
 
-    constructor(step : VerhaltStep, obj : VerhaltObjectModel) {
+    constructor(step : VerhaltStep, flag : string, obj : VerhaltObjectModel) {
         this._step = step;
+        this._flag = flag;
         this._obj = obj;
     }
 
