4ce3c0abf41d5f08b84da1e517411d010bc50548
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 18:57:53 +0300

    Add detailed types for VerPathStep interface, enhancing structure and content definitions

--- parser/core/path/verPathStep.d.ts
+++ parser/core/path/verPathStep.d.ts
@@ -1,3 +1,39 @@
 export interface IVerPathStep {
+    type : VerPathStepType
+    display : VerPathStepDisplay
+    content : VerPathStepContent
 
+    structure : VerPathStepStructure
+    catching : VerPathStepCatching
+    useRedirect : VerPathStepUseRedirect
 }
+
+export type VerPathStepType = VerPathStepObjectType | VerPathStepArrayType;
+
+export type VerPathStepObjectType = "object";
+
+export type VerPathStepArrayType = "array";
+
+export type VerPathStepDisplay = string;
+
+export type VerPathStepContent = VerPathStepObjectContent | VerPathStepArrayContent;
+
+export type VerPathStepObjectContent = string;
+
+export type VerPathStepArrayContent = number;
+
+export type VerPathStepStructure = VerPathStepStaticStructure | VerPathStepVariableStructure;
+
+export type VerPathStepStaticStructure = "static";
+
+export type VerPathStepVariableStructure = "variable";
+
+export type VerPathStepCatching = VerPathStepNativeCatching | VerPathStepOptionalCatching | VerPathStepStrictCatching;
+
+export type VerPathStepNativeCatching = "native";
+
+export type VerPathStepOptionalCatching = "optional";
+
+export type VerPathStepStrictCatching = "strict";
+
+export type VerPathStepUseRedirect = boolean;
