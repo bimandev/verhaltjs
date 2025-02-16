74d77fb8b35e93f7dc40978a88a71e19fe59edaa
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 13:02:45 +0300

    Add type and content properties to VerArithValue class

--- src/lib/arith/verArithValue.ts
+++ src/lib/arith/verArithValue.ts
@@ -1,3 +1,11 @@
+import { VerArithValueContent, VerArithValueType } from "./verArithValue.d";
+
 export class VerArithValue {
-    
+    type : VerArithValueType;
+    content : VerArithValueContent;
+
+    constructor(type : VerArithValueType, content : VerArithValueContent) {
+        this.type = type;
+        this.content = content;
+    }
 }
