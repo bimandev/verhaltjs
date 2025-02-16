85d492645398c9c687f48b9879d625030cda1a49
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 04:30:10 +0300

    Add link method to verhalt class for handling VerhaltObjectModel with options

--- lib/verhalt.ts
+++ lib/verhalt.ts
@@ -1,8 +1,11 @@
 import { parseRoutePaths, parsePathKeys, parseKey, parseKeyIndex } from "@verhalt/parser/lib"
 import { pathError } from "./utils/error";
+import { VerhaltLinkOptions, VerhaltObjectModel } from "@verhalt/types/lib";
 
 export class verhalt {
-
+    public static link(model : VerhaltObjectModel, path : string, options? : VerhaltLinkOptions) : any {
+        
+    }
 }
 
 export default verhalt;
