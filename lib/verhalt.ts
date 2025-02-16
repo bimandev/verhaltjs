33b7701833246bcd37f93813918f14dd0d7c53f1
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 05:00:57 +0300

    Implement link method in verhalt class to create VerhaltLink from model and path

--- lib/verhalt.ts
+++ lib/verhalt.ts
@@ -1,10 +1,61 @@
-import { parseRoutePaths, parsePathKeys, parseKey, parseKeyIndex } from "@verhalt/parser/lib"
-import { pathError } from "./utils/error";
-import { VerhaltLinkOptions, VerhaltObjectModel } from "@verhalt/types/lib";
+import { parsePathKeys, parseKey, parseKeyIndex } from "@verhalt/parser/lib"
+import { VerhaltArrayObject, VerhaltKey, VerhaltLink, VerhaltLinkOptions, VerhaltObjectModel, VerhaltPointer, VerhaltReference, VerhaltStep, VerhaltStructureObject } from "@verhalt/types/lib";
 
 export class verhalt {
-    public static link(model : VerhaltObjectModel, path : string, options? : VerhaltLinkOptions) : any {
-        
+    public static link(model : VerhaltObjectModel, path : string, options? : VerhaltLinkOptions) : VerhaltLink {
+        const origin : VerhaltReference = new VerhaltReference(null as unknown as VerhaltStep, model);
+        const pointers : VerhaltPointer[] = [];
+
+        const keys = parsePathKeys(path);
+        let target : VerhaltObjectModel = model;
+
+        while(keys.length > 0) {
+            const key : VerhaltKey = parseKey(keys.shift() as string) as VerhaltKey;
+            const references : VerhaltReference[] = [];
+
+            for(const step of key.steps) {
+                if(typeof target !== "object") throw new Error("Invalid Target: target is not an object");
+                const isArray = Array.isArray(target);
+
+                let raw : string;
+                switch(step.structure) {
+                    case "variable": {
+                        raw = ""; // TODO: Implement variable structure
+                        break;
+                    }
+                    default:
+                    case "static": {
+                        raw = step.content as string;
+                        break;
+                    }
+                }
+
+                let value : string | number;
+                switch(step.form) {
+                    case "name": {                        
+                        if(isArray) throw new Error("Invalid Target: Target is an array");
+
+                        value = raw;
+                        target = (target as VerhaltStructureObject)[value]
+                        break;
+                    }
+                    default:
+                    case "index": {
+                        if(!isArray) throw new Error("Invalid Target: Target is not an array");
+
+                        value = Number(raw);
+                        target = (target as VerhaltArrayObject)[value]
+                        break;
+                    }
+                }
+
+                references.push(new VerhaltReference(step, target));
+            }
+
+            pointers.push(new VerhaltPointer(key, references));
+        }
+
+        return new VerhaltLink(origin, path, options ?? {}, pointers);
     }
 }
 
