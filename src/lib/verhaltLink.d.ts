a93087f1cbf07e640bddf47dc4668d75cf14e68c
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 04:21:59 +0300

    Remove includeOrigin option from VerhaltLinkOptions for streamlined reference handling

--- src/lib/verhaltLink.d.ts
+++ src/lib/verhaltLink.d.ts
@@ -1,13 +1,10 @@
 export type VerhaltLinkOptions = Readonly<{
     referenceHandling : VerhaltLinkReferenceHandlingOption
-    includeOrigin : VerhaltLinkIncludeOriginOption    
     includeAllKeys : VerhaltLinkIncludeKeysOption
     includeSteps : VerhaltLinkIncludeStepsOption 
 }>;
 
 export type VerhaltLinkReferenceHandlingOption = "default" | "source";
-
-export type VerhaltLinkIncludeOriginOption = boolean;
 
 export type VerhaltLinkIncludeKeysOption = boolean;
 
