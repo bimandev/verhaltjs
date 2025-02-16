786015f084f2a24a5c06a1473c57e572ad88cd17
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 04:08:21 +0300

    Refactor VerhaltReference to a class structure with private properties and getter methods for step and obj

--- src/lib/verhaltReference.ts
+++ src/lib/verhaltReference.ts
@@ -1,9 +1,20 @@
-import { VerhaltKey } from "./verhaltKey";
 import { VerhaltObjectModel } from "./verhaltModel";
 import { VerhaltStep } from "./verhaltStep";
 
-export type VerhaltReference = {
-    key : VerhaltKey
-    step : VerhaltStep
-    obj : VerhaltObjectModel
+export class VerhaltReference {
+    private _step : VerhaltStep
+    private _obj : VerhaltObjectModel
+
+    constructor(step : VerhaltStep, obj : VerhaltObjectModel) {
+        this._step = step;
+        this._obj = obj;
+    }
+
+    public get step() : VerhaltStep {
+        return this._step;
+    }
+
+    public get obj() : VerhaltObjectModel {
+        return this._obj;
+    }
 }
