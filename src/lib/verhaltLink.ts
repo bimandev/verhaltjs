b8b4d2479c21444c1d73342b61836695b1958db1
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 03:42:42 +0300

    Enhance VerhaltLink class with origin and parent references, and improve current reference handling

--- src/lib/verhaltLink.ts
+++ src/lib/verhaltLink.ts
@@ -5,21 +5,42 @@ export class VerhaltLink {
     #refs : ReadonlyArray<VerhaltReference>;
     #options : VerhaltLinkOptions;
 
+    #origin: VerhaltReference;
+    #parent: VerhaltReference | undefined;
+    #current: VerhaltReference;
+
     constructor(refs : VerhaltReference[], options : VerhaltLinkOptions) {
         this.#refs = refs;
         this.#options = options;
+
+        this.#origin = refs[0];
+
+        switch(this.#options.referenceHandling) {
+            case "default":
+                this.#current = this.#refs[this.#refs.length - 1];
+                this.#parent = this.#refs[this.#refs.length - 2];
+                break;
+            case "source":
+                this.#current = this.#refs[refs.length - 1];
+                this.#parent = refs.length > 1 ? refs[refs.length - 2] : undefined;
+                break;
+        }
     }
 
     public get refs() : ReadonlyArray<VerhaltReference> {
         return this.#refs;
     }
 
-    public get origin() : VerhaltReference | undefined {
-        if(this.#options.includeOrigin) {
-            return this.#refs[0];
-        }
+    public get options() : VerhaltLinkOptions {
+        return this.#options;
+    }
 
-        return undefined;
+    public get origin() : VerhaltReference {
+        return this.#origin;
+    }
+
+    public get source() : VerhaltReference | undefined {
+        return this.#refs[this.#refs.length - 2];
     }
 
     public get current() : VerhaltReference {
