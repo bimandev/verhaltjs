225fb3a401c2fef3e14eb2f8209fdade6f1eef99
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 03:13:56 +0300

    Add VerhaltLinkOptions type with origin, keys, and steps properties

---
+++ src/lib/verhaltLink.d.ts
@@ -0,0 +1,11 @@
+export type VerhaltLinkOptions = {
+    origin : VerhaltLinkOriginOption    
+    keys : VerhaltLinkKeysOption
+    steps : VerhaltLinkStepsOption 
+}
+
+export type VerhaltLinkOriginOption = boolean;
+
+export type VerhaltLinkKeysOption = boolean;
+
+export type VerhaltLinkStepsOption = boolean;
