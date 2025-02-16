8ba4389a8988e20f34e314d42b5da3da67ecdea6
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 03:22:28 +0300

    Add constructor to VerhaltLink class to initialize refs and options

--- src/lib/verhaltLink.ts
+++ src/lib/verhaltLink.ts
@@ -1,3 +1,12 @@
-export class VerhaltLink {
+import { VerhaltLinkOptions } from "./verhaltLink.d";
+import { VerhaltReference } from "./verhaltReference";
 
+export class VerhaltLink {
+    #refs : VerhaltReference[];
+    #options : VerhaltLinkOptions;
+
+    constructor(refs : VerhaltReference[], options : VerhaltLinkOptions) {
+        this.#refs = refs;
+        this.#options = options;
+    }
 }
