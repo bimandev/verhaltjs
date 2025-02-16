382781a21ea106feaeb1b920d35a14225c1dd2d0
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 18:20:05 +0300

    Remove VerhaltKey type definitions and add VerPathKey type definitions

---
+++ types/core/path/verPathKey.d.ts
@@ -0,0 +1,22 @@
+import { VerhaltStep } from "./verhaltStep";
+import { VerPathStep } from "./verPathStep";
+
+export type VerPathKey = {
+    form : VerPathKeyForm
+    steps : VerPathKeySteps
+    catching : VerPathKeyCatching
+};
+
+export type VerPathKeyForm = VerPathKeyRootForm | VerPathKeyExtensionForm;
+
+export type VerPathKeyRootForm = "root";
+
+export type VerPathKeyExtensionForm = "extension";
+
+export type VerPathKeySteps = VerPathStep[];
+
+export type VerPathKeyCatching = VerPathKeyNativeCatching | VerPathKeyToleratedCatching;
+
+export type VerPathKeyNativeCatching = "native";
+
+export type VerPathKeyToleratedCatching = "tolerated";
