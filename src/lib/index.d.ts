7537f3c59d88fd819c8877e2fd0665c4fa55e4ea
k4yr2 <kyrzkaya@gmail.com>
2025-02-16 05:44:11 +0300

    Bump version to 1.8.5 in package.json and export verhaltReference in index.d.ts

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -4,4 +4,5 @@ export * from "./verhaltModel.d";
 export * from "./verhaltLink.d";
 export * from "./verhaltLink";
 export * from "./verhaltPointer";
+export * from "./verhaltReference.d";
 export * from "./verhaltReference";
