c0836bd7516865d5afbcca91db0cea545fc0bba7
k4yr2 <kyrzkaya@gmail.com>
2025-02-17 21:13:20 +0300

    Add keys property to VerhaltObjectPathPRS and refine input validation logic

--- parser/core/object/verhaltObjectPathPRS.ts
+++ parser/core/object/verhaltObjectPathPRS.ts
@@ -3,6 +3,8 @@ import { VerhaltObjectParser } from "./verhaltObjectParser";
 import { SchnurBracketCounterSLT, SchnurBufferSLT } from "schnur/singletons";
 
 export class VerhaltObjectPathPRS extends VerhaltObjectParser {
+    keys : string[];
+
     private constructor(input : string) {
         super(input);
 
@@ -11,24 +13,23 @@ export class VerhaltObjectPathPRS extends VerhaltObjectParser {
             const bcounter = s.bcounter<SchnurBracketCounterSLT>();
             const buffer = s.buffer<SchnurBufferSLT>();
 
-            if(bcounter.balanced) {
+            if(context.cursor === 0) {
                 if(char.value === ":") {
-                    if(!buffer.enable) {
-                        buffer.enable = true;
-                    }
-                    else {
-                        throw new Error("Unexpected ':' character is must be in first position");
-                    }
+                    buffer.enable = true;
                 }
-                else if(char.value === ".") {
-                    if(buffer.enable) {
+                else {
+                    throw new Error("Unexpected character");
+                }
+            }
+            else {
+                if(bcounter.balanced) {
+                    if(char.value === ".") {
                         buffer.flush();
-                    }
-                    else {
-                        throw new Error("Unexpected character");
+                        this.keys.push(buffer.value);
                     }
                 }
-            }       
+            }
+    
         }, (f) => {return {
             [f.bcounter] : new SchnurBracketCounterSLT(),
             [f.buffer] : new SchnurBufferSLT({ active: false }),
