a8912ffd2e6871ca23c69e269f16c61b05dc433f
k4yr2 <kyrzkaya@gmail.com>
2025-02-17 20:33:42 +0300

    Update schnur dependency to version 1.2.2 and enhance VerhaltObjectPathPRS for improved input handling

--- parser/core/object/verhaltObjectPathPRS.ts
+++ parser/core/object/verhaltObjectPathPRS.ts
@@ -1,7 +1,30 @@
+import { Schnur } from "schnur";
 import { VerhaltObjectParser } from "./verhaltObjectParser";
+import { SchnurBracketCounterSLT, SchnurBufferSLT } from "schnur/singletons";
 
 export class VerhaltObjectPathPRS extends VerhaltObjectParser {
     constructor(input : string) {
         super(input);
+
+        Schnur.parse(input, (char, s) => {
+            const bcounter = s.bcounter<SchnurBracketCounterSLT>();
+            const buffer = s.buffer<SchnurBufferSLT>();
+
+            if(bcounter.balanced) {
+                if(char.value === ":") {
+                    if(!buffer.enable) {
+                        buffer.enable = true;
+                    }
+                    else {
+                        throw new Error("Unexpected ':' character is must be in first position");
+                    }
+                }
+            }
+
+            
+        }, (f) => {return {
+            [f.bcounter] : new SchnurBracketCounterSLT(),
+            [f.buffer] : new SchnurBufferSLT({ active: false }),
+        }});
     }
 }
