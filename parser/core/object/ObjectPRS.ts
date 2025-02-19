eb2189272b56a1178db38752d6621cb93176cacb
k4yr2 <k4yr225@gmail.com>
2025-02-19 21:41:17 +0300

    Refactor ObjectPRS to use ObjectKeyPRS instead of ObjectPathKeyPRS for improved key handling

--- parser/core/object/ObjectPRS.ts
+++ parser/core/object/ObjectPRS.ts
@@ -1,23 +1,23 @@
 import { SchnurParser, SchnurParserSource } from "schnur/parsers";
 import { SchnurBufferSLT, SchnurUseParserSLT } from "schnur/singletons";
-import { ObjectPathKeyPRS } from "./ObjectKeyPRS";
+import { ObjectKeyPRS } from "./ObjectKeyPRS";
 
 export class ObjectPRS extends SchnurParser {
-    keys : ObjectPathKeyPRS[] = [];
+    keys : ObjectKeyPRS[] = [];
 
     private constructor(source : SchnurParserSource) {
         super(source, () => ({
-            useKey: (source) => SchnurUseParserSLT.create(source, ObjectPathKeyPRS.create),
+            useKey: (source) => SchnurUseParserSLT.create(source, ObjectKeyPRS.create),
         }));
     }
 
     protected awake(): void {
-        const useKey = this.sl.useKey<SchnurUseParserSLT<ObjectPathKeyPRS>>();
+        const useKey = this.sl.useKey<SchnurUseParserSLT<ObjectKeyPRS>>();
         useKey.start(false);
     }
 
     protected handle(): void | boolean {
-        const useKey = this.sl.useKey<SchnurUseParserSLT<ObjectPathKeyPRS>>();
+        const useKey = this.sl.useKey<SchnurUseParserSLT<ObjectKeyPRS>>();
         const context = this.context;
         const char = context.targetChar;
         
@@ -27,9 +27,9 @@ export class ObjectPRS extends SchnurParser {
     }
 
     protected finalize(): void {
-        const useKey = this.sl.useKey<SchnurUseParserSLT<ObjectPathKeyPRS>>();
+        const useKey = this.sl.useKey<SchnurUseParserSLT<ObjectKeyPRS>>();
 
-        this.keys = useKey.history as ObjectPathKeyPRS[];
+        this.keys = useKey.history as ObjectKeyPRS[];
         for(const key of this.keys) {
             const buffer = key.sl.buffer<SchnurBufferSLT>();
             console.log(buffer.stash);
