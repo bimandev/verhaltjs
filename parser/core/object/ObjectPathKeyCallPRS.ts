86ff7fa7b195ae6eae9f81745c38f42024f342d4
k4yr2 <k4yr225@gmail.com>
2025-02-19 20:58:26 +0300

    Add ObjectPathKeyPosPRS class for handling positional key parsing

---
+++ parser/core/object/ObjectPathKeyCallPRS.ts
@@ -0,0 +1,47 @@
+import { SchnurParser, SchnurParserSource } from "schnur/parsers";
+import { SchnurBufferSLT } from "schnur/singletons";
+
+export class ObjectPathKeyPosPRS extends SchnurParser {
+    content : string = "";
+
+    private constructor(source : SchnurParserSource) {
+        super(source, (f) => ({
+            [f.buffer] : SchnurBufferSLT.create
+        }));
+    }
+
+    private get buffer() : SchnurBufferSLT {
+        return this.sl.buffer<SchnurBufferSLT>();
+    }
+
+    protected handle(): void | boolean {
+        const context = this.context;
+        const char = context.targetChar;
+
+        if(this.order === 0) {
+            if(char.isOpenRoundBracket) {
+                return;
+            }
+            else {
+                throw new Error("Key pos must start with '(' character.");
+            }
+        }
+        else {
+            if(char.isCloseRoundBracket) {
+                return true;
+            }      
+        }
+
+        this.buffer.append();
+    }
+
+    protected finalize(): void {
+        const buffer = this.sl.buffer<SchnurBufferSLT>();
+        this.content = buffer.stash[0];
+    }
+
+
+    public static create(source : SchnurParserSource) : ObjectPathKeyPosPRS {
+        return new ObjectPathKeyPosPRS(source);
+    }
+}
