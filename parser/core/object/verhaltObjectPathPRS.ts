5e3020338b986c8cc0cf128f5644629fbb1094d9
k4yr2 <k4yr225@gmail.com>
2025-02-19 05:59:07 +0300

    Refactor VerhaltObjectPathPRS to extend SchnurParser and remove VerhaltObjectParser

--- parser/core/object/verhaltObjectPathPRS.ts
+++ parser/core/object/verhaltObjectPathPRS.ts
@@ -1,43 +1,21 @@
 import { Schnur } from "schnur";
-import { VerhaltObjectParser } from "./verhaltObjectParser";
-import { SchnurBracketCounterSLT, SchnurBufferSLT } from "schnur/singletons";
+import { SchnurParser } from "schnur/parsers";
 
-export class VerhaltObjectPathPRS extends VerhaltObjectParser {
+export class VerhaltObjectPathPRS extends SchnurParser {
     keys : string[] = [];
 
-    private constructor(input : string) {
-        super(input);
-
-        Schnur.parse(input, (context, s) => {
-            const char = context.char!;
-            const bcounter = s.bcounter<SchnurBracketCounterSLT>();
-            const buffer = s.buffer<SchnurBufferSLT>();
-
-            if(context.cursor === 0) {
-                if(char.value === ":") {
-                    buffer.enable = true;
-                }
-                else {
-                    throw new Error("Unexpected character");
-                }
-            }
-            else {
-                if(bcounter.balanced) {
-                    if(char.value === "." || context.cursor === context.content.length - 1) {
-                        buffer.flush();
-                        this.keys.push(buffer.value);
-                    }
-                }
-            }
-
-            console.log(char.value, buffer.flushed, buffer.current);
-        }, (f) => {return {
-            [f.bcounter] : new SchnurBracketCounterSLT(),
-            [f.buffer] : new SchnurBufferSLT({ active: false }),
-        }});
+    protected handle(): void | boolean {
+        const context = this.context;
+        const char = context.targetChar;
+        
+        console.log(char.value);
     }
 
-    public static from(input : string) : VerhaltObjectPathPRS {
-        return new VerhaltObjectPathPRS(input);
+
+
+
+
+    public static create() : VerhaltObjectPathPRS {
+        return new VerhaltObjectPathPRS();
     }
 }
