d7f38b55b9bfc8d1940fd8b62adbae08ba1e6931
k4yr2 <k4yr225@gmail.com>
2025-02-19 20:27:52 +0300

    Enhance ObjectPathKeyPRS to support useAcc and manage accessors with improved error handling

--- parser/core/object/ObjectPathKeyPRS.ts
+++ parser/core/object/ObjectPathKeyPRS.ts
@@ -1,22 +1,51 @@
 import { SchnurParser, SchnurParserSource } from "schnur/parsers";
-import { SchnurBufferSLT } from "schnur/singletons";
+import { SchnurBufferSLT, SchnurUseParserSLT } from "schnur/singletons";
+import { ObjectPathKeyAccPRS } from "./ObjectPathKeyAccPRS";
 
 export class ObjectPathKeyPRS extends SchnurParser {
+    root : boolean;
+    accs : ObjectPathKeyAccPRS[];
+
     private constructor(source : SchnurParserSource) {
         super(source, (f) => ({
-            [f.buffer] : SchnurBufferSLT.create
+            [f.useAcc] : (s) => SchnurUseParserSLT.create(s, ObjectPathKeyAccPRS.create)
         }));
+    }
+
+    private get useAcc() : SchnurUseParserSLT<ObjectPathKeyAccPRS> {
+        return this.sl.useAcc<SchnurUseParserSLT<ObjectPathKeyAccPRS>>();
     }
 
     protected handle(): void | boolean {
         const context = this.context;
         const char = context.targetChar;
-        const buffer = this.sl.buffer<SchnurBufferSLT>();
 
-        buffer.append();
-        console.log(char.value);
+        if(this.order === 0) {
+            if(char.value === ":") {
+                this.root = true;
+                return;
+            }
+            else if(char.value === ".") {
+                this.root = false;
+                return;
+            }
+            else {
+                throw new Error("Key must start with ':' or '.' character.");
+            }
+        }
+        else {
+            if(char.isOpenBracket) {            
+
+            }
+            else {
+                this.useAcc.start(false);
+            }
+        }
     }
 
+    protected finalize(): void {
+        this.accs = this.useAcc.history;
+    }
 
     public static create(source : SchnurParserSource) : ObjectPathKeyPRS {
         return new ObjectPathKeyPRS(source);
